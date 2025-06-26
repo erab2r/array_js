0c77af4f9908dfcd270116ad13d102ff32a25b18
erab2r <shkera213@gmail.com>
2025-06-26 16:17:58 +0600

    first commit

---
+++ ar_traversal.js
@@ -0,0 +1,8 @@
+const friends =['era','habib','khadiza','mohima','robin'];
+for(const friend of friends){
+
+}
+for(let i =0;i<friends.length;i++){
+    console.log(i);
+    console.log(friends[i]);
+}
