0a779992a748e19fcea6dfb7e618e179b57ef6b2
erab2r <shkera213@gmail.com>
2025-06-26 21:24:52 +0600

    gfhdg

--- revers.js
+++ revers.js
@@ -1,4 +1,10 @@
 const numbers = [1,2,3,44,5,6,7,8,9,10];
 console.log(numbers);
-numbers.reverse();
-console.log(numbers);
+// numbers.reverse();
+// console.log(numbers);
+
+const rev_numbers = [];
+for(const num of numbers){
+    rev_numbers.unshift(num);
+}
+    console.log(rev_numbers);
