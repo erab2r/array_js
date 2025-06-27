7249b2fb7a2d71752adbc683f0e20cab22a65647
erab2r <shkera213@gmail.com>
2025-06-27 13:17:16 +0600

    hnkljl

---
+++ fun-square.js
@@ -0,0 +1,18 @@
+function square(number){
+    console.log('Value of the number parameter :',number);
+    const borgo = number * number;
+     console.log('Square of the given number is :',borgo);
+
+}
+square(12);
+console.log('......................');
+square(13);
+console.log('......................');
+square(14);
+console.log('......................');
+square(15);
+console.log('......................');
+square(16);
+console.log('......................');
+square(17);
+console.log('......................');
