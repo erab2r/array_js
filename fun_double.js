b980d132cc2ef57a4e76b102d8c329bf45267ee5
erab2r <shkera213@gmail.com>
2025-06-27 13:39:17 +0600

    adfas

---
+++ fun_double.js
@@ -0,0 +1,11 @@
+function doubleIt(number){
+    const double = number * 2;
+    console.log(number,double);
+}
+console.log('I will call the Function');
+doubleIt(12);
+console.log('..................');
+doubleIt(15);
+console.log('..................');
+doubleIt(100);
+console.log('..................');
