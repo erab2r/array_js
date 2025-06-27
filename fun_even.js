cd5de3c389342dd2aa2d9f28a634bbe3a1098d05
erab2r <shkera213@gmail.com>
2025-06-27 22:15:28 +0600

    fdgs

---
+++ fun_even.js
@@ -0,0 +1,11 @@
+function isEven(number){
+    if(number%2===0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+console.log(isEven(5));
+console.log(isEven(12));
+console.log(isEven(11));
---
+++ fun_odd.js
@@ -0,0 +1,13 @@
+function isOdd(number){
+    if(number%2===1){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+console.log(isOdd(2));
+console.log(isOdd(3));
+console.log(isOdd(4));
+console.log(isOdd(5));
+console.log(isOdd(7));
