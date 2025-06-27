40b9d6858f9500204c51b4766fdadea885f9e78b
erab2r <shkera213@gmail.com>
2025-06-27 21:39:56 +0600

    m

---
+++ fun_return..js
@@ -0,0 +1,9 @@
+function doMath(num1,num2){
+    const sum = num1 + num2;
+    const sub = num1 - num2;
+    const mul = sum * sub;
+    const div = mul / 2;
+    return div;
+}
+const result =doMath(10,5);
+console.log(result);
