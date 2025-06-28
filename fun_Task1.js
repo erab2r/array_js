0e44aa4a9b2b5cdc85e8a8575b8b6a40f603ed9f
erab2r <shkera213@gmail.com>
2025-06-28 13:31:19 +0600

    aa

---
+++ fun_Task1.js
@@ -0,0 +1,8 @@
+// Take four parameters. Multiply the four numbers and then return the result
+
+function multiplyNumber(num1,num2,num3,num4){
+    const result = num1*num2*num3*num4;
+    return result;
+}
+const total = multiplyNumber(4,4,4,4);
+console.log(total);
