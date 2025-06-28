003bfdc210708e144858e4eedae2728f8dbb7583
erab2r <shkera213@gmail.com>
2025-06-28 13:36:46 +0600

    asdaf

---
+++ fun_Task2.js
@@ -0,0 +1,17 @@
+// Take a number if the number is odd multiply it by 2 and return the result. If the number is even divide it by two and return the result.
+
+function evenOddNumber(number){
+    if(number%2===1){
+        let result = number*2;
+        return result;
+    }
+    else{
+        let result = number*3;
+        return result;
+    }
+
+}
+
+console.log(evenOddNumber(5));
+console.log(evenOddNumber(6));
+console.log(evenOddNumber(7));
