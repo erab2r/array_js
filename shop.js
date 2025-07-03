7b86c9e85959c31403e8bccc9518a5a7ef22d752
erab2r <shkera213@gmail.com>
2025-07-04 00:33:32 +0600

    123

---
+++ shop.js
@@ -0,0 +1,17 @@
+const products = [
+    {name: 'Shampoo',price: 300,quantity: 2},
+    {name: 'Hair oil',price: 700,quantity: 3},
+    {name: 'LAFZ',price: 400,quantity: 6},
+    {name: 'REVIVE',price: 400,quantity: 1},
+]
+function cartTotal(products){
+    let total = 0;
+    for(const product of products){
+        const thisProductCost = product.price * product.quantity;
+        total = total + thisProductCost;
+    }
+    return total;
+
+}
+const shoppingCost = cartTotal(products);
+console.log(shoppingCost);
