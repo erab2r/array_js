ac3201955a1e845cd1ec1c17125ae518743fdf3a
erab2r <shkera213@gmail.com>
2025-07-05 21:33:10 +0600

    sd

---
+++ searchproduct.js
@@ -0,0 +1,13 @@
+const products = [
+   {id: 1,name: 'xiami phone',price: 19000},
+   {id: 2,name: 'iphone',price: 1000000},
+   {id: 3,name: 'mac book air',price: 119000},
+   {id: 4,name: 'lenavo laptop',price: 79000},
+   {id: 5,name: 'Dell inspiron laptop',price: 89000},
+   {id: 6,name: 'Samsung phone note 7',price: 19000},
+   {id: 7,name: 'Nokia phone',price: 19000},
+   {id: 8,name: 'phone one',price: 19000},
+]
+for(const product of products){
+    console.log(product);
+}
