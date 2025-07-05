efdac471d2354f96e43e153655d7e7a0540a20bd
erab2r <shkera213@gmail.com>
2025-07-05 13:31:26 +0600

    g

---
+++ obj.js
@@ -0,0 +1,14 @@
+const student = {
+    name : 'Sakib Khan',
+    id: 123,
+    address : 'Movie cinema',
+    isSingle : true,
+    friends : ['shihab','Amir','salman','raaz'],
+    car : {
+        brand : 'tesla',
+        price : 500000000,
+        made : 2020, 
+
+    }
+}
+console.log(student);
